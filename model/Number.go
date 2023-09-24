9e2d18bce1bd02ba2cdb678a0aa46eb0f61c2abc
yxchain <yxchain@hotmail.com>
2023-09-24 15:17:27 +0800

    The Addition of Decimals

--- model/Number.go
+++ model/Number.go
@@ -18,6 +18,14 @@ func CreateNumber(value string) (*Number, error) {
 	}
 	length := len(value)
 	dotIndex := strings.Index(value, ".")
+	if dotIndex < 0 {
+		n := new(Number)
+
+		n.digits, _ = CreateBinaryInteger(value)
+		n.scale = 0
+
+		return n, nil
+	}
 
 	var startIndex = 0
 	var sign = signPositive
@@ -40,4 +48,28 @@ func CreateNumber(value string) (*Number, error) {
 	n.scale = length - dotIndex - 1
 
 	return n, nil
+}
+
+func (n1 *Number) Add(n2 *Number) *Number {
+	tenBin, _ := CreateBinaryInteger("10")
+
+	b1 := n1.digits
+	b2 := n2.digits
+
+	if n1.scale < n2.scale {
+		for m := n1.scale; m < n2.scale; m++ {
+			b1 = b1.Multiply(tenBin)
+		}
+	} else if n1.scale > n2.scale {
+		for m := n2.scale; m < n1.scale; m++ {
+			b2 = b2.Multiply(tenBin)
+		}
+	}
+
+	n := new(Number)
+
+	n.digits = b1.Add(b2)
+	n.scale = bigger(n1.scale, n2.scale)
+
+	return n
 }
--- model/Number_test.go
+++ model/Number_test.go
@@ -9,3 +9,12 @@ func TestCreateNumber(t *testing.T) {
 	_, e = CreateNumber("123e4")
 	t.Log(e)
 }
+
+func TestNumber_Add(t *testing.T) {
+	n1, _ := CreateNumber("+13413.123")
+	n2, _ := CreateNumber("-13413")
+
+	n := n1.Add(n2)
+	t.Log(n.scale)
+	t.Log(n.digits)
+}
